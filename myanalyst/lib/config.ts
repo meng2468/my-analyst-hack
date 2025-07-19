f510eaee587c0d31b4348e6a1c0a27f0d2160648
meng2468 <mandm116@gmail.com>
2025-07-20 00:45:45 +0200

    Remove myanalyst

--- myanalyst/lib/config.ts
+++
@@ -1,5 +0,0 @@
-export const getBackendUrl = () => {
-  // In development, use localhost:7860
-  // In production, you can set this via environment variable
-  return process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:7860';
-}; 
